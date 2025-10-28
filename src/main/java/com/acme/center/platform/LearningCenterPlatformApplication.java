47efee4d2741e40a3e7024e76dd89152b747a415
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-28 10:57:06 -0500

    refactor: change main method visibility to static in learning center platform application.

--- src/main/java/com/acme/center/platform/LearningCenterPlatformApplication.java
+++ src/main/java/com/acme/center/platform/LearningCenterPlatformApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearningCenterPlatformApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(LearningCenterPlatformApplication.class, args);
     }
 
