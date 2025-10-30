ef6157d4a39f0dbc5cf7d15b8f38b104fe06c0fb
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-29 20:35:25 -0500

    feat(resources): add message-resource record for message handling.

---
+++ src/main/java/com/acme/center/platform/shared/interfaces/rest/resources/MessageResource.java
@@ -0,0 +1,8 @@
+package com.acme.center.platform.shared.interfaces.rest.resources;
+
+/**
+ * Message resource.
+ * @param message The message to show
+ */
+public record MessageResource(String message) {
+}
