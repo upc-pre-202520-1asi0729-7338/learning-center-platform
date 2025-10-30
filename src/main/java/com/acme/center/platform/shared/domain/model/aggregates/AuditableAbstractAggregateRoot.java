90b3f0c759157effcc91bedb4b8cc672e2fb855e
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-29 20:23:41 -0500

    feat(auditing): add base classes for auditable aggregate roots and model entities.

---
+++ src/main/java/com/acme/center/platform/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
@@ -0,0 +1,41 @@
+package com.acme.center.platform.shared.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AbstractAggregateRoot;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * Base class for aggregate roots that need to be audited.
+ * @param <T> The concrete type of the aggregate root.
+ * @summary
+ * The class is an abstract class that extends the {@link AbstractAggregateRoot} class.
+ */
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class AuditableAbstractAggregateRoot<T extends AbstractAggregateRoot<T>> extends AbstractAggregateRoot<T> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    /**
+     * Add a domain event to the aggregate root.
+     * @param event The event to add.
+     */
+    public void addDomainEvent(Object event) {
+        registerEvent(event);
+    }
+}
---
+++ src/main/java/com/acme/center/platform/shared/domain/model/entities/AuditableModel.java
@@ -0,0 +1,30 @@
+package com.acme.center.platform.shared.domain.model.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * Base class for model entities that need to be audited.
+ */
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class AuditableModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
