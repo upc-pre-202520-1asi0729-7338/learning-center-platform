21f06d6484c0b64784f31725620aa1e7e57d1255
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-04 11:28:00 -0500

    chore: update project version to 1.0.0 and enhance project description.

--- src/main/java/com/acme/center/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/acme/center/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -42,19 +42,7 @@ public class OpenApiConfiguration {
                         .description("ACME Learning Platform wiki Documentation")
                         .url("https://acme-learning-platform.wiki.github.io/docs"));
 
-        // Add a security scheme
 
-        final String securitySchemeName = "bearerAuth";
-
-        openApi.addSecurityItem(new SecurityRequirement()
-                        .addList(securitySchemeName))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
 
         // Return the OpenAPI configuration object with all the settings
 
