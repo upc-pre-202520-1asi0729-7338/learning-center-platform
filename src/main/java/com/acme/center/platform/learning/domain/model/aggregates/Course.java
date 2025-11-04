5328b025309a8dd1dbe8f2adafa4689f54d8da35
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-04 11:14:47 -0500

    feat(course): enhance course aggregate with learning path management and tutorial addition methods.

--- src/main/java/com/acme/center/platform/learning/domain/model/aggregates/Course.java
+++ src/main/java/com/acme/center/platform/learning/domain/model/aggregates/Course.java
@@ -1,7 +1,10 @@
 package com.acme.center.platform.learning.domain.model.aggregates;
 
 import com.acme.center.platform.learning.domain.model.commands.CreateCourseCommand;
+import com.acme.center.platform.learning.domain.model.valueobjects.LearningPath;
+import com.acme.center.platform.learning.domain.model.valueobjects.TutorialId;
 import com.acme.center.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import org.apache.logging.log4j.util.Strings;
@@ -11,23 +14,54 @@ import org.apache.logging.log4j.util.Strings;
 public class Course extends AuditableAbstractAggregateRoot<Course> {
     private String title;
     private String description;
+    @Embedded
+    private final LearningPath learningPath;
 
+    /**
+     * Create a new course
+     */
     public Course() {
-        super();
         this.title = Strings.EMPTY;
         this.description = Strings.EMPTY;
+        learningPath = new LearningPath();
     }
 
+    /**
+     * Create a new course with the given title and description
+     * @param title The title of the course
+     * @param description The description of the course
+     */
     public Course updateInformation(String title, String description) {
         this.title = title;
         this.description = description;
         return this;
     }
 
+    /**
+     * Create a new course with information from the command
+     * @param command The command to create the course
+     * @see CreateCourseCommand
+     */
     public Course(CreateCourseCommand command) {
-        this();
         this.title = command.title();
         this.description = command.description();
+        learningPath = new LearningPath();
     }
 
+    /**
+     * Add a tutorial to the learning path
+     * @param tutorialId The id of the tutorial
+     */
+    public void addTutorialToLearningPath(TutorialId tutorialId) {
+        this.learningPath.addItem(this, tutorialId);
+    }
+
+    /**
+     * Add a tutorial to the learning path
+     * @param tutorialId The id of the tutorial
+     * @param nextTutorialId The id of the next tutorial
+     */
+    public void addTutorialToLearningPath(TutorialId tutorialId, TutorialId nextTutorialId) {
+        this.learningPath.addItem(this, tutorialId, nextTutorialId);
+    }
 }
