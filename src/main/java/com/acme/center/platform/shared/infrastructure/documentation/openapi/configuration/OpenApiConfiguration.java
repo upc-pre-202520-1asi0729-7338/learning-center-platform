cb1f7ea50e6f38e0658ee26ad0fb4d8c876a3aaa
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-04 11:11:50 -0500

    feat(openapi): enhance open-api configuration with security scheme and documentation updates.

--- src/main/java/com/acme/center/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/acme/center/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,14 +1,19 @@
 package com.acme.center.platform.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfiguration {
+    // Properties
     @Value("${spring.application.name}")
     String applicationName;
 
@@ -18,20 +23,41 @@ public class OpenApiConfiguration {
     @Value("${documentation.application.version}")
     String applicationVersion;
 
+    // Methods
+
+    @Bean
     public OpenAPI learningPlatformOpenApi() {
-        var openApi = new OpenAPI();
+
 
         // General configuration
+        var openApi = new OpenAPI();
         openApi
                 .info(new Info()
                         .title(this.applicationName)
                         .description(this.applicationDescription)
                         .version(this.applicationVersion)
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")))
                 .externalDocs(new ExternalDocumentation()
-                        .description("ACME Learning Platform Wiki Documentation")
+                        .description("ACME Learning Platform wiki Documentation")
                         .url("https://acme-learning-platform.wiki.github.io/docs"));
-        // Return the OpenAPI configuration instance
+
+        // Add a security scheme
+
+        final String securitySchemeName = "bearerAuth";
+
+        openApi.addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+
+        // Return the OpenAPI configuration object with all the settings
+
         return openApi;
     }
 }
