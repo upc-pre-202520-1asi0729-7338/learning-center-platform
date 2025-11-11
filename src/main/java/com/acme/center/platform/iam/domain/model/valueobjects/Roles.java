ba18cb48f103ad331207b7885a3ab58cae4d5da2
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-11 11:29:29 -0500

    feat(iam): add user aggregate and related command/query classes.

---
+++ src/main/java/com/acme/center/platform/iam/domain/model/valueobjects/Roles.java
@@ -0,0 +1,16 @@
+package com.acme.center.platform.iam.domain.model.valueobjects;
+
+/**
+ * Roles
+ * <p>
+ *     This enum represents the roles in the system.
+ * </p>
+ */
+public enum Roles {
+    /** The user role. */
+    ROLE_USER,
+    /** The admin role. */
+    ROLE_ADMIN,
+    /** The instructor role. */
+    ROLE_INSTRUCTOR
+}
